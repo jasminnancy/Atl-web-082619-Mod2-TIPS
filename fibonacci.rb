01c08763c66e30fcb1644d879a9480a1432e06e8
Alexandria <alex.pugia816@hotmail.com>
2019-09-23 15:01:41 -0400

    done

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,9 +1,19 @@
-require 'pry-byebug'
+require 'pry'
 # Pry is included in case you'd like to run this file and test it,
 # but you may remove it if you like.
 
-def fibonacci(n)
-  # your code here :)
-end
+  def fibonacci(n)
+    sequence = []
+    sequence << n
+    sequence << (n + n)
+    y = 0
+    z = 1
+      10.times do
+        sequence << (sequence[y] + sequence[z])
+        y += 1
+        z += 1
+      end
+    puts sequence
+  end
 
-Pry.start
+  fibonacci(1)
