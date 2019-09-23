146ccf1f42d1d3b62a5505c41c39d0bfe51f5e3d
Alexandria <alex.pugia816@hotmail.com>
2019-09-23 15:33:42 -0400

    added commented out refactor

--- fibonacci.rb
+++ fibonacci.rb
@@ -17,3 +17,16 @@ require 'pry'
   end
 
   fibonacci(1)
+
+
+  # def fibonacci(n)
+  #   sequence = [1, 1]
+  #   y = 0
+  #   z = 1
+  #     (n - 2).times do
+  #       sequence << (sequence[y] + sequence[z])
+  #       y += 1
+  #       z += 1
+  #     end
+  #   puts sequence.last
+  # end
